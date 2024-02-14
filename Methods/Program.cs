a409485c4d779d08d05b4fe35d9ea56d130fa737
Çetin Yılmaz <143901863+ctnylmz@users.noreply.github.com>
2024-02-14 17:33:47 +0300

    Create Conditionals

---
+++ Methods/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          var result =  Calculate(10 , 20);
+          
+          Add(result);
+       
+        
+        }
+
+
+        static void Add(int add)
+        {
+            Console.WriteLine("Added " + add);
+        }
+
+        static int Calculate(int number, int number2)
+        {
+            return number + number2;
+        }
+
+
+        static int Calculate(int number, int number2, int number3)
+        {
+            return number + number2 + number3;
+        }
+
+    }
+}
