088a40c59870d134490ef811c0b76e4a205dca24
Çetin Yılmaz <143901863+ctnylmz@users.noreply.github.com>
2024-02-20 16:50:36 +0300

    1. lesson

---
+++ Workaround/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Workaround
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Engin Hocanın 1:23 Dk Kaldım");
+        }
+
+        public class Vatandas
+        {
+            public string Ad { get; set; }
+            public string Soyad { get; set; }
+            public int DogumYılı { get; set; }
+            public long TcNo { get; set; }
+        }
+    }
+}
