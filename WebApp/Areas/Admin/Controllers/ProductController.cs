20885e92e7c5fb7e55f19454b14865e92f7313cd
Çetin Yılmaz <143901863+ctnylmz@users.noreply.github.com>
2024-04-02 16:36:50 +0300

    Admin UI Crud Product

---
+++ WebApp/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,151 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Text;
+using WebApp.Areas.Admin.Models;
+
+namespace WebApp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ProductController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44380/api/Product/GetProductDetails");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ProductViewModel>>(JObject.Parse(jsonData)["data"].ToString());
+
+                return View(values);
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44380/api/Category/GetList");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<CategoryViewModel>>(JObject.Parse(jsonData)["data"].ToString());
+
+                var productViewModel = new ProductViewModel
+                {
+                    Categories = values
+                };
+
+                return View(productViewModel);
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(ProductViewModel product)
+        {
+
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(product);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:44380/api/Product/Add", stringContent);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:44380/api/Product/Get/{id}");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var service = JsonConvert.DeserializeObject<ProductViewModel>(JObject.Parse(jsonData)["data"].ToString());
+
+                var deleteData = JsonConvert.SerializeObject(service);
+                var stringContent = new StringContent(deleteData, Encoding.UTF8, "application/json");
+                var responseDelete = await client.PostAsync("https://localhost:44380/api/Product/Delete", stringContent);
+
+                if (responseDelete.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+
+            return View();
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Update(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:44380/api/Product/Get/{id}");
+            var responseCategory = await client.GetAsync("https://localhost:44380/api/Category/GetList");
+
+            if (responseMessage.IsSuccessStatusCode && responseCategory.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var service = JsonConvert.DeserializeObject<ProductViewModel>(JObject.Parse(jsonData)["data"].ToString());
+
+                var jsonDataCategory = await responseCategory.Content.ReadAsStringAsync();
+                var categories = JsonConvert.DeserializeObject<List<CategoryViewModel>>(JObject.Parse(jsonDataCategory)["data"].ToString());
+
+                var updateViewModel = new ProductViewModel
+                {
+                    id = service.id,
+                    categoryId = service.categoryId,
+                    categoryName = service.categoryName,
+                    name = service.name,
+                    description = service.description,
+                    price = service.price,
+                    imageUrl = service.imageUrl,
+                    status = service.status,
+                    Categories = categories
+                };
+
+                return View(updateViewModel);
+            }
+
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Update(ProductViewModel service)
+        {
+
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(service);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:44380/api/Product/Update", stringContent);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+    }
+}
